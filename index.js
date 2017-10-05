4c821de4e7de1f8ffcfe0f00ae7d620db8e53e6b
Nathan Campbell <nathancampbell@outlook.com>
2017-10-05 20:01:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+function showRepositories(event, data) {
+  console.log(this.responseText)
+}
+
 function getRepositories() {
   const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
   req.open("GET", 'https://api.github.com/users/chipsaboy/repos')
   req.send()
 }
 
-function showRepositories(event, data) {
-  console.log(this.responseText)
-}
